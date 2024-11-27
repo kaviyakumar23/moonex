34ee536f7f1d06450510af26d7ce0e897d495214
kaviyakumar23 <kaviyakumar23@gmail.com>
2024-11-27 21:25:47 +0530

    reduce foreground image size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,24 @@ import ellipseBgBottom from "../assets/ellipsebottom.png";
 
 const Hero = () => {
   return (
-    <div className="relative h-[110vh]">
+    <div className="relative h-[110vh] ">
+      <div className="absolute h-[110vh] z-[10] inset-0 bg-[url('/src/assets/hero/circles.png')] bg-cover bg-center bg-no-repeat pointer-events-none" />
+      <img
+        src={"/src/assets/hero/sphere.png"}
+        className="hidden md:block absolute z-[10] h-40 lg:h-52 right-16 xl:right-48 lg:right-28 top-60 lg:top-80"
+      />
+      <img
+        src={"/src/assets/hero/star-1.png"}
+        className="absolute z-[10] w-6 md:w-auto top-40 md:top-64 left-10 md:left-48"
+      />
+      <img
+        src={"/src/assets/hero/star-1.png"}
+        className="absolute z-[10] w-6 md:w-auto bottom-40 md:bottom-80 right-8 md:right-60"
+      />
+      <img
+        src={"/src/assets/hero/star-2.png"}
+        className="absolute z-[10] w-6 md:w-auto right-10 md:right-60 top-20 md:top-40"
+      />
       <div className="absolute z-10 top-0 left-0">
         <img src={ellipseBgTop} className="opacity-30" />
       </div>
