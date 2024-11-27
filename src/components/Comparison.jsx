1adbd9cddbfdf02af0120805db1c5c98d82b5d10
kaviyakumar23 <kaviyakumar23@gmail.com>
2024-11-27 19:35:46 +0530

    fix comparison table responsiveness

--- src/components/Comparison.jsx
+++ src/components/Comparison.jsx
@@ -27,67 +27,68 @@ const Comparison = () => {
       </div>
       <SectionHeader text="Why MoonEX?" />
 
-      <div className="p-4 mdx:p-6">
+      <div className="p-2 sm:p-4">
         <Card className="bg-white/5 backdrop-blur-md border-none shadow-2xl">
           <CardContent className="p-0">
-            <div className="overflow-x-auto">
-              <div className="min-w-[768px]">
-                {" "}
-                <div className="grid grid-cols-3 p-3 lg:p-7">
-                  <div className="px-6 py-6 lg:py-0 border-b border-r border-custom-border flex items-center justify-center">
-                    <h2 className="text-xl lg:text-2xl font-bold text-custom-header font-audiowide">
-                      Comparison
-                    </h2>
-                  </div>
-                  <div className="px-6 border-b border-r border-custom-border flex items-center justify-center">
-                    <div className="flex items-center space-x-2">
-                      <img
-                        src={moonexLogo}
-                        alt="MoonEX Logo"
-                        className="h-12 lg:h-auto"
-                      />
-                    </div>
-                  </div>
-                  <div className="px-6 border-b border-custom-border flex items-center justify-center">
-                    <div className="flex items-center space-x-2">
-                      <img
-                        src={UniSwapLogo}
-                        alt="Uniswap Logo"
-                        className="h-8 lg:h-auto"
-                      />
-                    </div>
-                  </div>
-                  {comparisonPoints.map((point, index) => (
-                    <Fragment key={index}>
-                      <div
-                        className={`py-6 px-6 xl:pl-16 border-r border-custom-border ${
-                          index < comparisonPoints.length - 1 ? "border-b" : ""
-                        }`}
-                      >
-                        <p className="text-gray-300 font-fira text-sm lg:text-base text-left whitespace-normal">
-                          {`${index + 1}. ${point}`}
-                        </p>
-                      </div>
-                      <div
-                        className={`p-6 border-r border-custom-border ${
-                          index < comparisonPoints.length - 1 ? "border-b" : ""
-                        } flex justify-center`}
-                      >
-                        <img src={check} className="w-5 h-5 lg:w-6 lg:h-6" />
-                      </div>
-                      <div
-                        className={`p-6 ${
-                          index < comparisonPoints.length - 1
-                            ? "border-b border-custom-border"
-                            : ""
-                        } flex justify-center`}
-                      >
-                        <img src={close} className="w-5 h-5 lg:w-6 lg:h-6" />
-                      </div>
-                    </Fragment>
-                  ))}
+            <div className="grid grid-cols-3 p-2 sm:p-3 lg:p-7 ">
+              <div className="px-2 sm:px-6 py-3 sm:py-6 lg:py-0 border-b border-r border-custom-border flex items-center justify-center">
+                <h2 className="text-xs sm:text-xl lg:text-2xl font-bold text-custom-header font-audiowide">
+                  Comparison
+                </h2>
+              </div>
+              <div className="px-2 sm:px-6 border-b border-r border-custom-border flex items-center justify-center">
+                <div className="flex items-center justify-center">
+                  <img
+                    src={moonexLogo}
+                    alt="MoonEX Logo"
+                    className="h-8 sm:h-8 lg:h-12"
+                  />
                 </div>
               </div>
+              <div className="px-2 sm:px-6 border-b border-custom-border flex items-center justify-center">
+                <div className="flex items-center justify-center">
+                  <img
+                    src={UniSwapLogo}
+                    alt="Uniswap Logo"
+                    className="h-5 sm:h-6 lg:h-8"
+                  />
+                </div>
+              </div>
+              {comparisonPoints.map((point, index) => (
+                <Fragment key={index}>
+                  <div
+                    className={`py-3 sm:py-6 px-2 sm:px-6 border-r border-custom-border ${
+                      index < comparisonPoints.length - 1 ? "border-b" : ""
+                    }`}
+                  >
+                    <p className="text-gray-300 font-fira text-xs sm:text-sm lg:text-base text-left">
+                      {`${index + 1}. ${point}`}
+                    </p>
+                  </div>
+                  <div
+                    className={`p-2 sm:p-6 border-r border-custom-border ${
+                      index < comparisonPoints.length - 1 ? "border-b" : ""
+                    } flex justify-center`}
+                  >
+                    <img
+                      src={check}
+                      className="w-4 h-4 sm:w-5 sm:h-5 lg:w-6 lg:h-6"
+                    />
+                  </div>
+                  <div
+                    className={`p-2 sm:p-6 ${
+                      index < comparisonPoints.length - 1
+                        ? "border-b border-custom-border"
+                        : ""
+                    } flex justify-center`}
+                  >
+                    <img
+                      src={close}
+                      className="w-4 h-4 sm:w-5 sm:h-5 lg:w-6 lg:h-6"
+                    />
+                  </div>
+                </Fragment>
+              ))}
             </div>
           </CardContent>
         </Card>
